d5d24debb4a689dd1374aa6f4411acf701ac35bf
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2024-07-20 17:42:26 +0530

    Setup todoapp project using Maven Fixes #5

---
+++ jee/todo-app/src/main/java/ols2406/todoapp/Authenticate.java
@@ -0,0 +1,14 @@
+package ols2406.todoapp;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Authenticate extends HttpServlet {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+    }
+}
