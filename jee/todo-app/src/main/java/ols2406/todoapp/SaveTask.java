b7cf8d044b474aa3a689ed0ea8c2a523b0f7eb53
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2024-08-02 16:54:54 +0530

    Setup todoapp project using Maven Fixes #5

---
+++ jee/todo-app/src/main/java/ols2406/todoapp/SaveTask.java
@@ -0,0 +1,13 @@
+package ols2406.todoapp;
+
+import java.io.IOException;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class SaveTask extends HttpServlet {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        
+    }
+}
